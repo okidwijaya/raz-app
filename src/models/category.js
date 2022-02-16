46c6032941587dc860b66ff37dd1fa09799e4215
special-snowflake <fzn475@gmail.com>
2022-02-17 02:03:48 +0700

    add brand, category, category quantity, add new product, fixing static

--- src/models/category.js
+++ src/models/category.js
@@ -1,4 +1,5 @@
 const db = require('../config/db');
+const mysql = require('mysql');
 
 const getListCategory = () => {
   return new Promise((resolve, reject) => {
@@ -27,4 +28,44 @@ const getListCategory = () => {
     });
   });
 };
-module.exports = {getListCategory};
+
+const getCategoryQuantity = (query) => {
+  return new Promise((resolve, reject) => {
+    console.log(query);
+    const {sort} = query;
+    let sqlSort = 'totalProduct DESC';
+    if (sort && sort.toLowerCase() === 'category asc') {
+      sqlSort = 'c.category ASC';
+    }
+    if (sort && sort.toLowerCase() === 'category desc') {
+      sqlSort = 'c.category DESC';
+    }
+    const sql = `SELECT c.id, c.category, count(cp.idProduct) as totalProduct 
+    FROM category c LEFT JOIN category_product cp 
+    ON cp.idCategory = c.id GROUP BY c.id ORDER BY ?`;
+    db.query(sql, [mysql.raw(sqlSort)], (err, result) => {
+      console.log(err);
+      if (err) {
+        return reject({
+          status: 500,
+          err: {msg: 'Something went wrong', data: null},
+        });
+      }
+      if (result.length === 0) {
+        return reject({
+          status: 404,
+          err: {msg: `Category can't be found`, data: null},
+        });
+      }
+      return resolve({
+        status: 200,
+        result: {
+          msg: `List of Categories Quantity`,
+          data: result,
+        },
+      });
+    });
+  });
+};
+
+module.exports = {getListCategory, getCategoryQuantity};
--- src/routers/products.js
+++ src/routers/products.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const productsRouter = express.Router();
 const auth = require('../middlewares/authorize');
+const uploads = require('../middlewares/uploadMulti');
 
 const productsController = require('../controllers/products');
 
-// productsRouter.post(
-//   '/add',
-//   auth.checkToken,
-//   auth.authorizeOwner,
-//   productsController.addProducts,
-// );
+productsRouter.post(
+  '/add',
+  auth.checkToken,
+  auth.authorizeOwner,
+  uploads,
+  productsController.addProducts,
+);
+
 productsRouter.get('/detail/:id', productsController.getDetailByID);
 productsRouter.get('/search', productsController.searchProducts);
+productsRouter.delete(
+  '/:id',
+  auth.checkToken,
+  auth.authorizeOwner,
+  productsController.deleteProduct,
+);
 
 module.exports = productsRouter;
